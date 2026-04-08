c17b79a44c82138cbb0f408683d3c9bc4474f9d6
Skyler Berry <skylerberry@unknown8aca3dc07684.attlocal.net>
2026-04-07 22:57:56 -0700

    v0.9.6: context menu shows Lock/Unlock based on selection state

--- src/components/CustomContextMenu.tsx
+++ src/components/CustomContextMenu.tsx
@@ -3,6 +3,7 @@ import {
   TldrawUiMenuGroup,
   TldrawUiMenuItem,
   useActions,
+  useEditor,
   type TLUiContextMenuProps,
 } from "tldraw";
 
@@ -20,11 +21,29 @@ function ActionItem({ actionId }: { actionId: string }) {
   );
 }
 
+function LockItem() {
+  const actions = useActions();
+  const editor = useEditor();
+  const action = actions["toggle-lock"];
+  const selectedShapes = editor.getSelectedShapes();
+  const allLocked = selectedShapes.length > 0 && selectedShapes.every((s) => s.isLocked);
+
+  return (
+    <TldrawUiMenuItem
+      id={action.id}
+      label={allLocked ? "Unlock" : "Lock"}
+      kbd={action.kbd}
+      icon={action.icon as string | undefined}
+      onSelect={action.onSelect}
+    />
+  );
+}
+
 export default function CustomContextMenu(props: TLUiContextMenuProps) {
   return (
     <DefaultContextMenu {...props}>
       <TldrawUiMenuGroup id="actions">
-        <ActionItem actionId="toggle-lock" />
+        <LockItem />
         <ActionItem actionId="duplicate" />
       </TldrawUiMenuGroup>
       <TldrawUiMenuGroup id="reorder">
